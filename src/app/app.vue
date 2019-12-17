700b2e04c552523f2ce11984d07ea07aa77658d0
Øystein Moseng <oystein@highsoft.com>
2019-12-17 12:23:14 +0100

    Added shell header and app component structure.

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,13 +1,31 @@
+<!--
+    App component is the root container for the app. It expands to the width and height of
+    the element it is placed within. See the "./public" directory for usage.
+-->
 <template>
-    <h1>Test</h1>
+    <div class="app-container">
+        <Header />
+    </div>
 </template>
 
 <script lang="ts">
+import Header from './components/Header.vue';
 
+export default {
+    components: {
+        Header
+    }
+};
 </script>
 
 <style lang="less" scoped>
-h1 {
-    color: red;
+@import "colors";
+
+.app-container {
+    background-color: @app-bg-color;
+    padding: 10px;
+    height: 100%;
+    width: 100%;
+    box-sizing: border-box;
 }
 </style>
---
+++ src/app/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <nav>Test</nav>
+    </header>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style lang="less" scoped>
+header {
+    width: 100%;
+    background-color: gray;
+}
+</style>
