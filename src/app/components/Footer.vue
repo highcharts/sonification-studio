7da72da38d1ad87366221b29adee87e7462c1962
Øystein Moseng <oystein@highsoft.com>
2019-12-18 10:29:19 +0100

    Added Footer component.

---
+++ src/app/components/Footer.vue
@@ -0,0 +1,54 @@
+<template>
+    <footer>
+        <div id="title">
+            <img
+                alt="Highcharts logo"
+                :src="highchartsIcon"
+            >
+            Chart Sonification Studio
+        </div>
+        <div id="copyright">
+            © Copyright {{ year }} Highsoft AS - designed in collaboration with Georgia Institue of Technology
+        </div>
+    </footer>
+</template>
+
+<script lang="ts">
+import highchartsIcon from '../assets/highcharts-logo.svg';
+
+export default {
+    data: function () {
+        return { highchartsIcon };
+    },
+    computed: {
+        year: () => new Date().getFullYear()
+    }
+};
+</script>
+
+<style lang="less" scoped>
+footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+#copyright {
+    font-size: 0.7em;
+    margin: 8px;
+}
+
+#title {
+    margin-top: 10px;
+    font-size: 0.8em;
+    letter-spacing: 0.1em;
+    display: flex;
+    align-items: center;
+    img {
+        width: 16px;
+        height: $width;
+        margin: 0 8px;
+    }
+}
+</style>
