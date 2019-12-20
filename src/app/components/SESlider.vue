ffc68061825e3430973be68317baa05220c6f814
Øystein Moseng <oystein@highsoft.com>
2019-12-20 15:17:50 +0100

    Fixed light version styling for SESlider.

--- src/app/components/SESlider.vue
+++ src/app/components/SESlider.vue
@@ -1,14 +1,16 @@
 <!--
     A reusable slider with consistent cross-browser styling.
-    Provide label for the slider in default slot.
+    Provide label for the slider in default slot. For external label,
+    provide empty slot and set the "labelledby" prop.
 
     Props:
-        - id: String - Id for the slider. Automatically set on the input control.
+        - [id]: String - Id for the slider. Automatically set on the input control.
         - [min]: Number - Min value for slider.
         - [max]: Number - Max value for slider.
         - [value]: Number - Current slider value (one-way binding only).
         - [step]: Number - Step for slider values.
         - [dark]: Boolean - Alternate styling for dark backgrounds.
+        - [labelledby]: String - Aria-labelledby for slider, in case of external label.
 -->
 <template>
     <div class="se-slider">
@@ -20,13 +22,14 @@
         </label>
         <div class="se-slider-inner">
             <input
-                :id="id"
+                :id="id || null"
                 type="range"
                 :min="min"
                 :max="max"
                 :value="value"
                 :step="step"
                 :class="{ dark: dark }"
+                :aria-labelledby="labelledby || null"
             >
         </div>
     </div>
@@ -35,12 +38,13 @@
 <script lang="ts">
 export default {
     props: {
+        id: { type: String, default: '' },
         dark: { type: Boolean, default: false },
         min: { type: Number, default: 0 },
         max: { type: Number, default: 100 },
         value: { type: Number, default: 50 },
         step: { type: Number, default: 1 },
-        id: { type: String, required: true },
+        labelledby: { type: String, default: '' }
     }
 };
 </script>
@@ -62,16 +66,17 @@ export default {
         height: 20px;
     }
 
-    .track(@color) {
+    .track(@color, @border-color) {
         width: 100%;
         height: 4px;
         cursor: pointer;
         border-radius: 5px;
+        border: 1px solid @border-color;
         background: @color;
     }
 
-    .thumb(@color) {
-        border: 1px solid @seslider-border-color;
+    .thumb(@color, @border-color) {
+        border: 1px solid @border-color;
         height: 16px;
         width: 16px;
         border-radius: 50%;
@@ -79,37 +84,37 @@ export default {
         cursor: pointer;
     }
 
-    .inputStyles(@inputColor) {
+    .inputStyles(@inputColor, @border-color) {
         &::-moz-range-track {
-            .track(@inputColor);
+            .track(@inputColor, @border-color);
         }
 
         &::-webkit-slider-runnable-track {
-            .track(@inputColor);
+            .track(@inputColor, @border-color);
             height: 5px;
         }
 
         &::-moz-range-thumb {
-            .thumb(@inputColor);
+            .thumb(@inputColor, @border-color);
         }
 
         &::-ms-thumb {
-            .thumb(@inputColor);
+            .thumb(@inputColor, @border-color);
         }
 
         &::-webkit-slider-thumb {
-            .thumb(@inputColor);
+            .thumb(@inputColor, @border-color);
             -webkit-appearance: none;
-            margin-top: -6px;
+            margin-top: -6.5px;
         }
 
         &::-moz-range-progress {
-            .track(@inputColor);
+            .track(@inputColor, @border-color);
             background: @seslider-progress-color;
         }
 
         &::-ms-track {
-            .track(@inputColor);
+            .track(@inputColor, @border-color);
             background: transparent;
             border-color: transparent;
             border-width: 16px 0;
@@ -133,10 +138,10 @@ export default {
         margin: 8px 0;
         width: 100%;
 
-        .inputStyles(@seslider-color);
+        .inputStyles(@seslider-color, @seslider-border-color);
 
         &.dark {
-            .inputStyles(@seslider-dark-color);
+            .inputStyles(@seslider-dark-color, @seslider-dark-border-color);
         }
     }
 
