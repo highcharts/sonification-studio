92ac94b027144c5b464fdcd3615b03a220ef7ecf
Øystein Moseng <oystein@highsoft.com>
2019-12-20 18:35:02 +0100

    Added helptext popup support to SEControl.

--- src/app/components/basic/SEControl.vue
+++ src/app/components/basic/SEControl.vue
@@ -23,9 +23,19 @@
             <button
                 v-if="helptext"
                 class="helpicon"
+                @click="helptextActive = !helptextActive"
+                @keyup.esc="helptextActive = false"
             >
                 ?
             </button>
+            <div
+                v-show="helptextActive"
+                class="helptext-popup"
+                @click="helptextActive = false"
+            >
+                <p>{{ helptext }}</p>
+                <div class="helptext-arrow" />
+            </div>
         </div>
         <div class="se-control-content">
             <slot />
@@ -40,6 +50,18 @@ export default {
         controlId: { type: String, required: true },
         helptext: { type: String, default: '' },
         horizontal: { type: Boolean, default: false }
+    },
+    data: function () {
+        return {
+            helptextActive: false
+        };
+    },
+    mounted: function () {
+        document.addEventListener('click', (e: MouseEvent) => {
+            if (!this.$el.contains(e.target as any)) {
+                this.helptextActive = false;
+            }
+        });
     }
 };
 </script>
@@ -65,12 +87,72 @@ export default {
         flex: 1;
     }
 
+    .se-control-label-container {
+        display: flex;
+        align-items: center;
+        position: relative;
+    }
+
     .se-control-label {
         margin: 3px;
     }
 
     .helpicon {
-        display: none;
+        margin: 2px;
+        margin-left: 4px;
+        padding-left: 0.5px;
+        width: 18px;
+        height: $width;
+        overflow: hidden;
+        background-color: @secontrol-helpicon-bg;
+        color: @secontrol-helpicon-color;
+        border: 1px solid @secontrol-helpicon-color;
+        border-radius: 50%;
+        font: inherit;
+        font-size: 13px;
+        text-align: center;
+        font-weight: bold;
+        cursor: pointer;
+        display: block;
+        &:hover {
+            background-color: @secontrol-helpicon-hover-bg;
+            color: @secontrol-helpicon-hover-color;
+            border-color: @secontrol-helpicon-hover-color;
+        }
+        &:active {
+            background-color: darken(@secontrol-helpicon-hover-bg, 5%);
+            color: darken(@secontrol-helpicon-hover-color, 5%);
+        }
+    }
+
+    .helptext-popup {
+        cursor: pointer;
+        position: absolute;
+        box-shadow: 0 1px 10px @secontrol-helptext-shadow;
+        width: 200px;
+        right: -89px;
+        bottom: 0;
+        font-size: 12px;
+        margin-bottom: 30px;
+        z-index: 99;
+        padding: 10px;
+        box-sizing: border-box;
+        background-color: @secontrol-helptext-bg;
+        color: @secontrol-helptext-color;
+        .helptext-arrow {
+            background-color: @secontrol-helptext-bg;
+            width: 16px;
+            height: $width;
+            position: absolute;
+            left: calc(50% - 8px);
+            bottom: -4px;
+            transform: rotate(45deg);
+            z-index: 98;
+        }
+        p {
+            z-index: 100;
+            position: relative;
+        }
     }
 
 </style>
