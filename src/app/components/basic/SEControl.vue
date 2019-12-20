b2c934e3782583cf540d722f89d4447191c7bb6b
Øystein Moseng <oystein@highsoft.com>
2019-12-20 17:14:51 +0100

    SEControl styling.

--- src/app/components/basic/SEControl.vue
+++ src/app/components/basic/SEControl.vue
@@ -4,7 +4,7 @@
 
     Props:
         - label: String - The label text to show.
-        - labelId: String - Id to place on the label.
+        - controlId: String - Id of the input control.
         - [helptext]: String - Show help icon with text popup.
         - [horizontal]: Boolean - Show label to the right of control, rather than above.
 -->
@@ -13,13 +13,23 @@
         class="se-control"
         :class="{ horizontal: horizontal }"
     >
-        <div class="se-control-label">
-            <p :id="labelId">
+        <div class="se-control-label-container">
+            <label
+                :for="controlId"
+                class="se-control-label"
+            >
                 {{ label }}
-            </p>
-            <p>?</p>
+            </label>
+            <button
+                v-if="helptext"
+                class="helpicon"
+            >
+                ?
+            </button>
         </div>
-        <slot />
+        <div class="se-control-content">
+            <slot />
+        </div>
     </div>
 </template>
 
@@ -27,7 +37,7 @@
 export default {
     props: {
         label: { type: String, required: true },
-        labelId: { type: String, required: true },
+        controlId: { type: String, required: true },
         helptext: { type: String, default: '' },
         horizontal: { type: Boolean, default: false }
     }
@@ -36,5 +46,31 @@ export default {
 
 <style lang="less" scoped>
     @import "../../colors";
+
+    .se-control {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+        &.horizontal {
+            flex-direction: row-reverse;
+            justify-content: flex-end;
+            .se-control-label {
+                margin-left: 10px;
+            }
+        }
+    }
+
+    .se-control-content {
+        flex: 1;
+    }
+
+    .se-control-label {
+        margin: 3px;
+    }
+
+    .helpicon {
+        display: none;
+    }
 
 </style>
