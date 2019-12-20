2424f67fddb142482ebae8890af39fa8d859c0ed
Øystein Moseng <oystein@highsoft.com>
2019-12-20 20:39:54 +0100

    Use SECheckbox.

--- src/app/components/MappingsColumnPan.vue
+++ src/app/components/MappingsColumnPan.vue
@@ -1,10 +1,27 @@
 <template>
     <div>
         <SEControl
-            control-id="myid"
-            label="Label goes here"
-            helptext="Helptext"
+            control-id="mcp-enable-pan-mapping"
+            label="Enable pan mapping"
+            helptext="Helptext for checkbox goes here"
             horizontal
+        >
+            <SECheckbox id="mcp-enable-pan-mapping" />
+        </SEControl>
+
+        <SEControl
+            control-id="mcp-enable-sweep"
+            label="Enable sweep"
+            helptext="Helptext for sweep option goes here"
+            horizontal
+        >
+            <SECheckbox id="mcp-enable-sweep" />
+        </SEControl>
+
+        <SEControl
+            control-id="myid"
+            label="Slider"
+            helptext="Helptext"
         >
             <SESlider id="myid" />
         </SEControl>
@@ -14,14 +31,19 @@
 <script lang="ts">
 import SEControl from './basic/SEControl.vue';
 import SESlider from './basic/SESlider.vue';
+import SECheckbox from './basic/SECheckbox.vue';
 
 export default {
     components: {
         SEControl,
-        SESlider
+        SESlider,
+        SECheckbox
     }
 };
 </script>
 
 <style lang="less" scoped>
+    .se-control {
+        margin: 10px 0;
+    }
 </style>
