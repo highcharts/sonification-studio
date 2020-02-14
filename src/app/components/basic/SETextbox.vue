fa8488699f2c8a29148511d8968888a1e8264682
Øystein Moseng <oystein@highsoft.com>
2020-02-14 18:54:14 +0100

    Add SETextbox basic component.

---
+++ src/app/components/basic/SETextbox.vue
@@ -0,0 +1,38 @@
+<!--
+    A reusable input textbox.
+
+    Props:
+        - id: String - Input element id.
+
+    Events:
+        - input: Re-emits the input event for use with v-model.
+-->
+<template>
+    <input
+        :id="id"
+        @input="$emit('input', $event.target.value)"
+    >
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        id: { type: String, required: true }
+    }
+};
+</script>
+
+<style lang="less" scoped>
+    @import "../../colors";
+
+    input {
+        margin-top: 4px;
+        box-sizing: border-box;
+        width: 100%;
+        font-size: 1.05em;
+        border: 1px solid @setextbox-border-color;
+        border-radius: 4px;
+        background-color: @setextbox-bg;
+        padding: 6px 9px;
+    }
+</style>
