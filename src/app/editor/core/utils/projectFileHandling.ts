83fbaf603a0ef4625ef5feea7e248c09a4841171
Øystein Moseng <oystein@highsoft.com>
2022-06-30 14:09:46 +0200

    Warn when loading old project files.

--- src/app/editor/core/utils/projectFileHandling.ts
+++ src/app/editor/core/utils/projectFileHandling.ts
@@ -5,9 +5,13 @@
 import { Store } from 'vuex';
 import { downloadURI } from './browserUtils';
 import LZString from './lz-string.min.js';
+import { storageRevision } from '../../store/store';
+import { version } from '../../../../../package.json';
 
 export function downloadProjectFile(projectTitle: string, store: Store<any>): void {
     const state = store.state;
+    state.appVersion = version;
+    state.storeVersion = storageRevision;
     const json = JSON.stringify(state);
     const compressed = LZString.compressToUTF16(json);
     const blob = new Blob([compressed], {type: 'application/octet-stream'});
@@ -20,6 +24,13 @@ export function restoreFromProjectFile(fileContents: string, store: Store<any>):
     const uncompressed = LZString.decompressFromUTF16(fileContents);
     if (uncompressed) {
         const obj = JSON.parse(uncompressed);
+        const storeVersion = obj.storeVersion;
+        if (!storeVersion || storeVersion < storageRevision) {
+            console.warn(`Loading project format version ${storeVersion}. Current version is ${storageRevision}.`);
+            alert('You are loading a project file from an older version. It may be incompatible with the current version. Please contact us for help with recovering old projects.');
+        }
+        delete obj.storeVersion;
+        delete obj.appVersion;
         store.dispatch('restoreState', obj);
     } else {
         throw new Error('Project file not valid.');
